4ec0be13755545bba8277351de314678a2ef10ab
Audun Hammer Hovda <audun@unicornis.no>
2023-08-14 17:51:05 +0200

    Flexy sponsors

--- components/Sponsor.vue
+++ components/Sponsor.vue
@@ -2,43 +2,41 @@
   <div class="bg-gray-900 py-24 sm:py-32">
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <h2 class="text-center text-lg font-semibold leading-8 text-white">
-        Trusted by the world’s most innovative teams
+        Våre samarbeidspartnere
       </h2>
-      <div
-        class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5"
-      >
+      <div class="mt-10 flex justify-center flex-wrap gap-8 sm:gap-10">
         <nuxt-img
-          class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          src="https://tailwindui.com/img/logos/158x48/transistor-logo-white.svg"
-          alt="Transistor"
+          class="invert max-h-12 object-contain"
+          src="https://wcte2023.org/wp-content/uploads/Oslo-logo-sort-CMYK.png"
+          alt="Oslo kommune"
           width="158"
           height="48"
         />
         <nuxt-img
-          class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          src="https://tailwindui.com/img/logos/158x48/reform-logo-white.svg"
-          alt="Reform"
+          class="invert max-h-12 object-contain"
+          src="https://wcte2023.org/wp-content/uploads/Oslo-logo-sort-CMYK.png"
+          alt="Oslo kommune"
           width="158"
           height="48"
         />
         <nuxt-img
-          class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          src="https://tailwindui.com/img/logos/158x48/tuple-logo-white.svg"
-          alt="Tuple"
+          class="invert max-h-12 object-contain"
+          src="https://wcte2023.org/wp-content/uploads/Oslo-logo-sort-CMYK.png"
+          alt="Oslo kommune"
           width="158"
           height="48"
         />
         <nuxt-img
-          class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-          src="https://tailwindui.com/img/logos/158x48/savvycal-logo-white.svg"
-          alt="SavvyCal"
+          class="invert max-h-12 object-contain"
+          src="https://wcte2023.org/wp-content/uploads/Oslo-logo-sort-CMYK.png"
+          alt="Oslo kommune"
           width="158"
           height="48"
         />
         <nuxt-img
-          class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-          src="https://tailwindui.com/img/logos/158x48/statamic-logo-white.svg"
-          alt="Statamic"
+          class="invert max-h-12 object-contain"
+          src="https://wcte2023.org/wp-content/uploads/Oslo-logo-sort-CMYK.png"
+          alt="Oslo kommune"
           width="158"
           height="48"
         />
@@ -46,3 +44,4 @@
     </div>
   </div>
 </template>
+<script setup lang="ts"></script>
