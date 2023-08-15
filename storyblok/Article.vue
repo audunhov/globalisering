47ca7e43d5dde8e9a275240893a0462be80f3345
Audun Hammer Hovda <audun@unicornis.no>
2023-08-15 18:53:43 +0200

    better page display

--- storyblok/Article.vue
+++ storyblok/Article.vue
@@ -1,26 +1,34 @@
 <template>
-  <div v-editable="blok">
-    <div
-      class="bg-primary relative h-64 isolate flex flex-col justify-end items-center"
-    >
+  <div v-editable="blok" class="grid grid-cols-1 lg:grid-cols-[400px_1fr]">
+    <div class="bg-primary hidden lg:block">
       <nuxt-img
-        class="w-full absolute -z-10 mx-auto h-64 object-cover"
+        class="h-full overflow-hidden object-cover rounded-tr-2xl hidden lg:block"
         :src="blok.image.filename"
         :alt="blok.image.alt"
       />
-      <div class="absolute inset-0 bg-gradient-to-t from-black/50 -z-10" />
-      <Transition
-        enter-from-class="-translate-y-full"
-        enter-active-class="transition ease-in-out duration-300"
-        appear
+    </div>
+    <div>
+      <div
+        class="bg-primary relative h-64 isolate flex flex-col justify-end items-center"
       >
+        <nuxt-img
+          class="w-full absolute -z-10 lg:hidden mx-auto h-64 object-cover"
+          :src="blok.image.filename"
+          :alt="blok.image.alt"
+        />
+        <div
+          class="absolute inset-0 lg:hidden bg-gradient-to-t from-black/50 -z-10"
+        />
+
         <h1 class="text-3xl font-bold text-white mb-10">
           {{ blok.title }}
         </h1>
-      </Transition>
-    </div>
+      </div>
 
-    <StoryblokComponent v-for="b in blok.body" :key="b._uid" :blok="b" />
+      <div class="p-5">
+        <StoryblokComponent v-for="b in blok.body" :key="b._uid" :blok="b" />
+      </div>
+    </div>
   </div>
 </template>
 
--- storyblok/Section.vue
+++ storyblok/Section.vue
@@ -1,15 +1,11 @@
 <template>
-  <div class="relative flex items-center min-h-[500px]">
-    <div
-      v-editable="blok"
-      class="z-10 w-full text-center relative"
-      :class="widths[blok.width]"
-    >
+  <div v-editable="blok" class="relative min-h-[500px] isolate">
+    <div class="h-full" :class="widths[blok.width]">
       <StoryblokComponent v-for="b in blok.content" :key="b._uid" :blok="b" />
     </div>
     <nuxt-img
       v-if="blok.background.filename"
-      class="absolute z-0 inset-0 w-full h-full object-cover"
+      class="absolute -z-10 inset-0 w-full h-full object-cover"
       :src="blok.background.filename"
       :alt="blok.background.alt"
     />
